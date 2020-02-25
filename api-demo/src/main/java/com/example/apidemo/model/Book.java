48855176c701b53e9a5e05744fec0f2b0dc5b3fc
unknown <g.mameniskis1@gmail.com>
2020-02-25 11:48:52 -0500

    working state

--- api-demo/src/main/java/com/example/apidemo/model/Book.java
+++ api-demo/src/main/java/com/example/apidemo/model/Book.java
@@ -1,13 +1,12 @@
 package com.example.apidemo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.util.HashMap;
 
 @Entity
 public class Book {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String kind;
     private String etag;
@@ -24,4 +23,85 @@ public class Book {
 
     @OneToOne(targetEntity = Book.class)
     private HashMap<String, String> searchInfo;
+
+    public Book() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getEtag() {
+        return etag;
+    }
+
+    public void setEtag(String etag) {
+        this.etag = etag;
+    }
+
+    public String getSelfLink() {
+        return selfLink;
+    }
+
+    public void setSelfLink(String selfLink) {
+        this.selfLink = selfLink;
+    }
+
+    public VolumeInfo getVolumeInfo() {
+        return volumeInfo;
+    }
+
+    public void setVolumeInfo(VolumeInfo volumeInfo) {
+        this.volumeInfo = volumeInfo;
+    }
+
+    public SaleInfo getSaleInfo() {
+        return saleInfo;
+    }
+
+    public void setSaleInfo(SaleInfo saleInfo) {
+        this.saleInfo = saleInfo;
+    }
+
+    public AccessInfo getAccessInfo() {
+        return accessInfo;
+    }
+
+    public void setAccessInfo(AccessInfo accessInfo) {
+        this.accessInfo = accessInfo;
+    }
+
+    public HashMap<String, String> getSearchInfo() {
+        return searchInfo;
+    }
+
+    public void setSearchInfo(HashMap<String, String> searchInfo) {
+        this.searchInfo = searchInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", kind='" + kind + '\'' +
+                ", etag='" + etag + '\'' +
+                ", selfLink='" + selfLink + '\'' +
+                ", volumeInfo=" + volumeInfo +
+                ", saleInfo=" + saleInfo +
+                ", accessInfo=" + accessInfo +
+                ", searchInfo=" + searchInfo +
+                '}';
+    }
 }
--- api-demo/src/main/java/com/example/apidemo/model/BookQuery.java
+++ api-demo/src/main/java/com/example/apidemo/model/BookQuery.java
@@ -12,4 +12,49 @@ public class BookQuery {
     private Integer totalItems;
     @OneToMany
     private List<Book> items;
+
+    public BookQuery() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public Integer getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(Integer totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public List<Book> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Book> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "BookQuery{" +
+                "id=" + id +
+                ", kind='" + kind + '\'' +
+                ", totalItems=" + totalItems +
+                ", items=" + items +
+                '}';
+    }
 }
